bb17fbd98a91e8bbe443f1aabeac001fe7271d31
Adam Gilbert <agilbertmath@gmail.com>
2024-11-11 18:04:40 -0500

    Motivating Cross-Validation RScript

--- ComparingTestRMSE.R
+++ ComparingTestRMSE.R
@@ -15,10 +15,12 @@ my_data <- tibble(
 
 #Split into training and test sets
 #Replace seed with your birthdate
-set.seed(06022024)
+set.seed(09302016)
 data_splits <- initial_split(my_data, prop = 0.75)
 train <- training(data_splits)
 test <- testing(data_splits)
+
+train_folds <- vfold_cv(train, v = 10)
 
 #Construct and fit model
 lr_spec <- linear_reg() %>%
@@ -26,22 +28,28 @@ lr_spec <- linear_reg() %>%
 
 #Uncomment the pipe and the polynomial feature engineering
 #step to build a higher-order model
-lr_rec <- recipe(y ~ x, data = train) #%>%
-  #step_poly(x, degree = 3, options = list(raw = TRUE))
+lr_rec <- recipe(y ~ x, data = train) %>%
+  step_poly(x, degree = 3, options = list(raw = TRUE))
 
 lr_wf <- workflow() %>%
   add_model(lr_spec) %>%
   add_recipe(lr_rec)
 
-lr_fit <- lr_wf %>%
-  fit(train)
+lr_cv_results <- lr_wf %>%
+  fit_resamples(train_folds)
 
-#Assess model on test data
-test_rmse <- lr_fit %>%
-  augment(test) %>%
-  rmse(y, .pred) %>%
-  pull(.estimate)
+# #Assess model on test data
+# test_rmse <- lr_fit %>%
+#   augment(test) %>%
+#   rmse(y, .pred) %>%
+#   pull(.estimate)
+# 
+# ##Share your test RMSE and what it suggests about your model's predictions...
+# print(paste0("Test RMSE: ", test_rmse))
+# print(paste0("We expect predictions accurate to within about: +/- ", 2*test_rmse))
 
-##Share your test RMSE and what it suggests about your model's predictions...
-print(paste0("Test RMSE: ", test_rmse))
-print(paste0("We expect predictions accurate to within about: +/- ", 2*test_rmse))
+lr_cv_results %>%
+  collect_metrics()
+
+# lr_cv_results %>%
+#   collect_metrics(summarize = FALSE)
